d5a8376af64b07cfbe8506db0614084dee19aba7
Skip Keats <wkea@loc.gov>
2023-11-24 15:33:23 -0500

    Backticks.

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,24 @@
 
 /* =========================================== */
 
-let condition = true;
-let person = "Bob";
-const age: number = 40;
+// let condition: boolean = true;
+// let person: string = "Bob";
+// const age: number = 40;
 
-let place;
-console.log("Place value: " + place + " Type: " + typeof(place));
-place = "London";
-console.log("Place value: " + place + " Type: " + typeof(place));
+// let place: string | undefined | null;
+// console.log("Place value: " + place + " Type: " + typeof(place));
+// place = "London";
+// console.log("Place value: " + place + " Type: " + typeof(place));
+// place = null;
+// console.log("Place value: " + place + " Type: " + typeof(place));
+
+/* ========================================= */
+
+let firstBool = true;
+let secondBool = false;
+
+let firstString = "This is a string";
+let secondString = 'And so is this';
+
+let place: string | undefined | null;
+console.log(`Place value: ${place} Type: ${typeof(place)}`);
