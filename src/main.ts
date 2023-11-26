4a27d8068b4c0339877fc10557b2f5ea5f0fb430
Skip Keats <wkea@loc.gov>
2023-11-26 10:19:17 -0500

    Equality conversion

--- src/main.ts
+++ src/main.ts
@@ -26,11 +26,65 @@
 
 /* ========================================= */
 
-let firstBool = true;
-let secondBool = false;
+// let firstBool = true;
+// let secondBool = false;
 
-let firstString = "This is a string";
-let secondString = 'And so is this';
+// let firstString = "This is a string";
+// let secondString = 'And so is this';
 
-let place: string | undefined | null;
-console.log(`Place value: ${place} Type: ${typeof(place)}`);
+// let place: string | undefined | null;
+// console.log(`Place value: ${place} Type: ${typeof(place)}`);
+
+/* ========================================= */
+
+// let daysInWeek = 7;
+// let pi = 3.14;
+// let hexValue = 0xFFFF;
+
+/* ========================================= */
+
+// let person1 = "Alice";
+// let person2: string | undefined = "Bob";
+
+/* ========================================= */
+
+// let firstName = "Adam";
+
+// if (firstName == "Adam") {
+//     console.log("firstName is Adam");
+// } else if (firstName == "Jacqui") {
+//     console.log("firstName is Jacqui");
+// } else {
+//     console.log("firstName is neither Adam nor Jacqui");
+// }
+
+// switch (firstName) {
+//     case "Adam":
+//         console.log("firstName is Adam");
+//         break;
+//     case "Jacqui":
+//         console.log("firstName is Jacqui");
+//         break;
+//     default:
+//         console.log("firstName is neither Adam nor Jacqui");
+//         break;
+// }
+
+/* ========================================= */
+
+// let firstVal = 5;
+// let secondVal = "5";
+
+// if (firstVal === secondVal) {
+//     console.log("They are the same");
+// } else {
+//     console.log("They are NOT the same");
+// }
+
+/* ========================================= */
+
+let myData1 = 5 + 5;
+let myData2 = 5 + "5";
+
+console.log(`Result 1: ${myData1}, Type: ${typeof(myData1)}`);
+console.log(`Result 2: ${myData2}, Type: ${typeof(myData2)}`);
