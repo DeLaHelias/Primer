7b3ea4dc7c43a22665af86e46ac36d4ae49e4300
Skip Keats <wkea@loc.gov>
2023-11-30 16:10:41 -0500

    Data typing

--- src/main.ts
+++ src/main.ts
@@ -83,7 +83,7 @@
 
 /* ========================================= */
 
-let myData1 = 5 + 5;
+let myData1 = (5).toString() + String(5);
 let myData2 = 5 + "5";
 
 console.log(`Result 1: ${myData1}, Type: ${typeof(myData1)}`);
