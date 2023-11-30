26c0a51a22dc83664ebd137567970569a74f1d80
Skip Keats <skip.keats@gmail.com>
2023-11-30 17:00:27 -0500

    Working with arrays, p 4.

--- src/main.ts
+++ src/main.ts
@@ -156,7 +156,4 @@
 
 /* ============================================ */
 
-let myArray: (number | string | boolean)[] = [];
-myArray[0] = 100;
-myArray[1] = "Adam";
-myArray[2] = true;
+let myArray: (number | string | boolean)[] = [100, "Adam", true];
