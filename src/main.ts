f3efbbfd55fc59daae15f09668f93d76358df699
Skip Keats <skip.keats@gmail.com>
2023-11-30 16:57:41 -0500

    Working with arrays, p 3.

--- src/main.ts
+++ src/main.ts
@@ -156,7 +156,7 @@
 
 /* ============================================ */
 
-let myArray: any[] = [];
+let myArray: (number | string | boolean)[] = [];
 myArray[0] = 100;
 myArray[1] = "Adam";
 myArray[2] = true;
